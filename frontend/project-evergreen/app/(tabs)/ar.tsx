73e9ad46ff122a4bc540e451bdaab7b776e5839e
abhay-byte <abhay02delhi@gmail.com>
2025-07-07 20:30:54 +0530

    revert point

--- frontend/project-evergreen/app/(tabs)/ar.tsx
+++ frontend/project-evergreen/app/(tabs)/ar.tsx
@@ -1,2 +1,51 @@
-jsx import { ARCore } from 'react-native-arkit'
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroImage,
+  ViroTrackingStateConstants,
+  ViroTrackingReason,
+} from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 
+const MyARScene = () => {
+  const [trackingStatus, setTrackingStatus] = useState(
+    ViroTrackingStateConstants.TRACKING_UNAVAILABLE
+  );
+
+  const onARInitialized = (
+    state: ViroTrackingStateConstants,
+    reason: ViroTrackingReason
+  ) => {
+    setTrackingStatus(state);
+  };
+
+  return (
+    <ViroARScene onTrackingUpdated={onARInitialized}>
+      {trackingStatus == ViroTrackingStateConstants.TRACKING_NORMAL && (
+        <ViroImage
+          height={0.5}
+          width={0.5}
+          source={"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"}
+          position={[0, 0, -1]}
+        />
+      )}
+    </ViroARScene>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: MyARScene,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+});
